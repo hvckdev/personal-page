99efa708bee5b5748c125c973c0c60109823524f
hvck <work@hvck.dev>
2023-05-27 21:06:57 +0400

    🥹 feat: Animation by ChatGPT

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@
         class="bg-black bg-opacity-25 backdrop-blur-md p-6 rounded-xl text-center max-w-md fade-in"
     >
       <div class="font-bold text-4xl mb-4">hvck</div>
+      <hr class="border-t border-gray-500 my-4">
       <p class="text-lg mb-4">
         Hello, I'm hvck, a backend developer with a passion for creating
         efficient and scalable systems. I have experience in various backend
@@ -15,12 +16,14 @@
         to learn more and improve my skills. In my free time, I enjoy exploring
         new technologies, reading, and hiking.
       </p>
+      <hr class="border-t border-gray-500 my-4">
       <div class="flex justify-center space-x-4 mb-4">
         <a href="https://t.me/hvckdev"
         ><img src="../assets/telegram-icon.svg" alt="Telegram" class="h-6 w-6"
         /></a>
         <a href="mailto:i@hvck.dev"><img src="../assets/email-icon.svg" alt="Email" class="h-6 w-6" /></a>
       </div>
+      <hr class="border-t border-gray-500 my-4">
       <p class="text-sm">Made with 💖 by ChatGPT - 2023</p>
     </div>
   </main>
