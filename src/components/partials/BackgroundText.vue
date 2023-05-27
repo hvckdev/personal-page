35026d11e6646fa45490a369ebec2ccda9875a3f
hvck <work@hvck.dev>
2023-05-27 20:47:24 +0400

    🥹 feat: Updates by ChatGPT

---
+++ src/components/partials/BackgroundText.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <h1 class="background-text break-all">
+    HVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCKHVCK
+  </h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,25 @@
 </script>
 
 <template>
-  <main class="bg-neutral-900">
-    <div class="grid h-screen place-items-center">
-    <div class="text-violet-400">
-      <span class="font-bold">hvck.dev</span> <span class="underline">/ 
-       <a class="underline" href="https://t.me/hvckdev">tg</a> /
-       <a class="underline" href="mailto:i@hvck.dev">mail</a> / 
-       <a class="underline" href="https://git.hvck.dev">git</a>
-       </span>
+  <main>
+    <div
+        class="bg-black bg-opacity-25 backdrop-blur-md p-6 rounded-xl text-center max-w-md"
+    >
+      <div class="font-bold text-4xl mb-4">hvck</div>
+      <p class="text-lg mb-4">
+        Hello, I'm hvck, a backend developer with a passion for creating
+        efficient and scalable systems. I have experience in various backend
+        technologies such as PHP, Laravel, Symfony, and Kafka. I'm always eager
+        to learn more and improve my skills. In my free time, I enjoy exploring
+        new technologies, reading, and hiking.
+      </p>
+      <div class="flex justify-center space-x-4 mb-4">
+        <a href="https://t.me/hvckdev"
+        ><img src="../assets/telegram-icon.svg" alt="Telegram" class="h-6 w-6"
+        /></a>
+        <a href="mailto:i@hvck.dev"><img src="../assets/email-icon.svg" alt="Email" class="h-6 w-6" /></a>
       </div>
+      <p class="text-sm">Made with 💖 by ChatGPT - 2023</p>
     </div>
   </main>
 </template>
