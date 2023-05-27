c286451c4d53504a5857a5a249f9d30125f6f513
hvck <work@hvck.dev>
2023-05-27 21:07:55 +0400

    🥹 feat: Animation by ChatGPT

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 </script>
 
-<template>э
+<template>
   <div class="bg-gradient-to-r from-purple-900 via-purple-800 to-purple-700 flex items-center justify-center h-screen text-white">
     <RouterView />
   </div>
