189823c60030c15cd0e38cecb8163c34e9e1b111
hvck <work@hvck.dev>
2023-06-14 20:30:41 +0400

    😍 feat: Text improvement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,9 +12,10 @@
               </span>
         </div>
 
-        <p class="pl-4 mb-4 text-lg border-l-4 text-left">
-          Hello, I'm
-          <span class="hs-tooltip">
+        <div class="pl-4 mb-4 text-lg border-l-4 text-left">
+          <p>
+            Hello, I'm
+            <span class="hs-tooltip">
             <span
                 class="hs-tooltip-toggle"
             >
@@ -27,14 +28,52 @@
               </span>
             </span>
           </span>
-          I'm a backend developer, I'm deeply committed to designing robust, scalable systems, leveraging my expertise
-          in PHP, Laravel, Symfony, and Kafka. My passion for technology doesn't stop at work. I enjoy delving into new
-          technologies, always eager to unearth their hidden gems.
-          <br>
-          From an early age, I embarked on my coding journey with C++, a path that nurtured my fondness for the
-          aesthetic appeal of computers and the rhythm of typing code. Alongside my tech pursuits, I explore psychology,
-          enriching my professional productivity and personal well-being through insightful literature.
-        </p>
+            a backend developer. Work with PHP: Symfony, Laravel.
+            I love interesting projects and i'm trying to make the
+            maximum contribution i can to them.
+            I'm not an expert but i'm not to afraid a JS: Vue.
+            I am always in the process of learning, including learning how this or that technology works under the hood.
+            Also i explore psychology that improve my productivity and alone happiness.
+          </p>
+          <p class="mt-2">
+            <a class="hs-collapse-toggle inline-flex items-center gap-x-2 font-medium hover:text-gray-400 dark:text-gray-400 dark:hover:text-white"
+               href="javascript:;"
+               id="hs-show-hide-collapse" data-hs-collapse="#hs-show-hide-collapse-heading">
+              <span class="hs-collapse-open:hidden">ask a chatgpt to tell about me</span>
+              <span class="hs-collapse-open:block hidden">omg hide this plssssss</span>
+              <svg class="hs-collapse-open:rotate-180 w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16"
+                   fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+                      stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+              </svg>
+            </a>
+          </p>
+          <div id="hs-show-hide-collapse-heading"
+               class="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300"
+               aria-labelledby="hs-show-hide-collapse">
+            <p class="text-gray-500">
+              Enthusiastically engaged in backend development, I pride myself on my ability to construct powerful,
+              scalable
+              systems. My technical acumen is bolstered by my proficiency in PHP, Laravel, Symfony, and Kafka. Not just
+              limited to using these technologies, I show a consistent curiosity about the inner workings of each
+              system,
+              regularly dedicating time to dissect existing technologies and understand their underlying mechanisms.
+              <br><br>
+              My fascination with coding was ignited at a young age, as I ventured into the world of programming through
+              the
+              gateway of C++. This early start has enabled me to develop an acute sensitivity towards the aesthetics of
+              computers and the very act of typing, which I perceive as a symphony of thought transformation into
+              action.
+              <br><br>
+              In addition to my technological pursuits, I have a profound interest in psychology. This parallel passion
+              allows me to apply psychological insights to enhance my workflow, boosting both my professional
+              productivity
+              and personal contentment. With such a unique blend of interests and skills, I bring a balanced,
+              well-rounded
+              perspective to every project I engage in.
+            </p>
+          </div>
+        </div>
 
         <div class="flex justify-center mb-4 space-x-4">
           <a href="https://t.me/hvckdev"><i class="fa-brands fa-telegram"></i></a>
