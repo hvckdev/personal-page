4aea1d1d70c13a6a8092c5d82fe183899f826ace
hvck <work@hvck.dev>
2023-09-18 19:39:12 +0400

    feat: purpleeeeeeeeeee

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
-<script setup>
-import NavbarMain from './components/partials/NavbarMain.vue';
-</script>
-
 <template>
   <div>
     <div>
-      <navbar-main class="sticky top-0"></navbar-main>
       <RouterView />
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,26 @@
 <template>
   <main class="text-white">
-    <div class="flex items-center justify-center h-[80vh]">
-      <div class="max-w-md m-auto p-6 text-center bg-black bg-opacity-25 backdrop-blur-md rounded-xl fade-in">
-        <div class="hs-tooltip">
-          <div class="hs-tooltip-toggle mb-4 text-4xl font-bold">hvck</div>
-          <span
-              class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 text-xs font-medium text-white rounded-md shadow-sm bg-purple-800 bg-opacity-25"
-              role="tooltip"
-          >
-                 pronounces as hack
-              </span>
+    <div class="flex items-center justify-center h-screen text-black">
+      <div>
+        <div class="mb-4 pl-3.5">
+          <img src="/src/assets/img/logo.png" class="w-36 h-auto" alt="hvck" />
         </div>
-
-        <div class="pl-4 mb-4 text-lg border-l-4 text-left">
-          <p>
-            Hello, I'm
-            <span class="hs-tooltip">
-            <span
-                class="hs-tooltip-toggle"
-            >
-              hvck,
-               <span
-                   class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 text-xs font-medium text-white rounded-md shadow-sm bg-purple-800 bg-opacity-25"
-                   role="tooltip"
-               >
-                 pronounces as hack
-              </span>
-            </span>
-          </span>
-            a backend engineer with a passion for creating
-            efficient and scalable systems. I'm always eager
-            to learn more and improve my skills. In my free time. I enjoy exploring
-            new technologies, reading, and hiking. Also i enjoy reading psychology books,
-            that improve my productivity.
-          </p>
+        <div class="flex justify-center m b-4 space-x-4 transition-all">
+          <a class="hover:text-purple-500" href="https://t.me/hvckdev"
+            ><i class="fa-brands fa-telegram"></i
+          ></a>
+          <a class="hover:text-purple-500" href="https://t.me/hvckxm"
+            ><i class="fa-solid fa-brain"></i
+          ></a>
+          <a class="hover:text-purple-500" href="https://github.com/hvckdev"
+            ><i class="fa-brands fa-github"></i
+          ></a>
+          <a class="hover:text-purple-500" href="mailto:i@hvck.dev"
+            ><i class="fa-solid fa-envelope"></i
+          ></a>
         </div>
-
-        <div class="flex justify-center mb-4 space-x-4 transition-all">
-          <a class="hover:text-emerald-400" href="https://t.me/hvckdev"><i class="fa-brands fa-telegram"></i></a>
-          <a class="hover:text-emerald-400"  href="https://t.me/hvckxm"><i class="fa-solid fa-brain"></i></a>
-          <a class="hover:text-emerald-400"  href="https://github.com/hvckdev"><i class="fa-brands fa-github"></i></a>
-          <a class="hover:text-emerald-400"  href="mailto:i@hvck.dev"><i class="fa-solid fa-envelope"></i></a>
-        </div>
-
-        <p class="text-sm">Made with 💜</p>
-        <p class="text-sm">2023</p>
       </div>
     </div>
   </main>
 </template>
-<script setup>
-</script>
+<script setup></script>
